1af32492332f6e7ff332238d47535eaf44f4ddf8
Pragyanshree Das <shree.pragyan23@gmail.com>
2022-08-08 14:37:44 +0530

    created data class in kotlin as representing model class of MVVM

---
+++ app/src/main/java/com/ocean/demokotlinretrofit/model/ResultsTrendingTvWeekModel.kt
@@ -0,0 +1,22 @@
+package com.ocean.demokotlinretrofit.model
+
+import com.google.gson.annotations.SerializedName
+
+data class ResultsTrendingTvWeekModel(
+
+    @SerializedName("adult"             ) var adult            : Boolean?          = null,
+    @SerializedName("backdrop_path"     ) var backdropPath     : String?           = null,
+    @SerializedName("id"                ) var id               : Int?              = null,
+    @SerializedName("name"              ) var name             : String?           = null,
+    @SerializedName("original_language" ) var originalLanguage : String?           = null,
+    @SerializedName("original_name"     ) var originalName     : String?           = null,
+    @SerializedName("overview"          ) var overview         : String?           = null,
+    @SerializedName("poster_path"       ) var posterPath       : String?           = null,
+    @SerializedName("media_type"        ) var mediaType        : String?           = null,
+    @SerializedName("genre_ids"         ) var genreIds         : ArrayList<Int>    = arrayListOf(),
+    @SerializedName("popularity"        ) var popularity       : Double?           = null,
+    @SerializedName("first_air_date"    ) var firstAirDate     : String?           = null,
+    @SerializedName("vote_average"      ) var voteAverage      : Double?           = null,
+    @SerializedName("vote_count"        ) var voteCount        : Int?              = null,
+    @SerializedName("origin_country"    ) var originCountry    : ArrayList<String> = arrayListOf()
+)
---
+++ app/src/main/java/com/ocean/demokotlinretrofit/model/TrendingTVWeekModel.kt
@@ -0,0 +1,11 @@
+package com.ocean.demokotlinretrofit.model
+
+import com.google.gson.annotations.SerializedName
+
+data class TrendingTVWeekModel(
+
+    @SerializedName("page"          ) var page         : Int?               = null,
+    @SerializedName("results"       ) var results      : ArrayList<ResultsTrendingTvWeekModel> = arrayListOf(),
+    @SerializedName("total_pages"   ) var totalPages   : Int?               = null,
+    @SerializedName("total_results" ) var totalResults : Int?               = null
+)
